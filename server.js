28cd30523f2b13e8589483cf844bb947d6893c02
kunjtapiawala <kunj8989@gmail.com>
2025-02-05 13:25:18 -0600

    Update start script and use process.env.PORT for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = 3000;
+// server.js
+const port = process.env.PORT || 3000;
+// ...
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
 
 // Import Arduino IoT client code
 const IotApi = require('@arduino/arduino-iot-client');
