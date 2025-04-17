b2747c6b588b83f653ff4ac117a6d44f323daeeb
kunjtapiawala <kunj8989@gmail.com>
2025-04-17 14:16:09 -0500

    fix: Update navigation layout and styling

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCWjnJMgmAs0P8F4ALePNuFI1mggA4snzw",
     authDomain: "freezesense-dabf4.firebaseapp.com",
@@ -16,8 +10,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const analytics = getAnalytics(app);
-
-export { auth }; 
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore(); 
